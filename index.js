4fde13cfac7cd8b1c3201e4cde96111b7c80bfad
john2796 <jbmiranda22796@gmail.com>
2019-02-11 11:36:42 -0800

    completed mvp

--- index.js
+++ index.js
@@ -6,11 +6,49 @@ const server = express();
 
 // middleware
 server.use(express.json());
-
+//READ
 server.get("/api/users", (req, res) => {
   db.find()
     .then(user => {
-      res.status(200).json(user);
+      res.status(200).json({ success: true, user });
+    })
+    .catch(err => res.status(400).json({ success: false, message: err }));
+});
+//POST
+server.post("/api/users", (req, res) => {
+  const user = req.body;
+  db.insert(user)
+    .then(user => {
+      res.status(200).json({ success: true, user });
+    })
+    .catch(err => res.status(400).json({ success: false, message: err }));
+});
+//DELETE
+server.delete("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  db.remove(id)
+    .then(user => {
+      res.status(200).json({ success: true });
+    })
+    .catch(err => res.status(400).json({ success: false, message: err }));
+});
+//DELETE
+server.put("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  const user = req.body;
+  db.update(id, user)
+    .then(updated => {
+      //check if id is there
+      if (updated) {
+        res.status(200).json({ success: true, updated });
+      } else {
+        res
+          .status(404)
+          .json({
+            success: false,
+            message: "I cannot find the hub you're looking for "
+          });
+      }
     })
     .catch(err => res.status(400).json({ success: false, message: err }));
 });
