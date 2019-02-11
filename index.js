fbb0054d7b950f99c107d0d8efa47a5e335aff49
john2796 <jbmiranda22796@gmail.com>
2019-02-11 11:24:04 -0800

    setup app and add get route for users

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // implement your API here
+const express = require("express");
+const port = 5000;
+const db = require("./data/db");
+const server = express();
+
+// middleware
+server.use(express.json());
+
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => res.status(400).json({ success: false, message: err }));
+});
+
+server.listen(port, () => {
+  console.log(`port running on ${port}`);
+});
